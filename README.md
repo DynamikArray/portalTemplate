a2520426aaded52e45ef8893e3c34a041b122f1f
Monumentix <brian@monumentixwebdesign.com>
2017-08-21 03:07:40 -0400

    ADDED: data

---
+++ README.md
@@ -0,0 +1,18 @@
+#Portal Template Site
+-bootstrapped
+
+## Node/Express backend
+-sessions
+
+## React Frontend
+-
+
+##.env file
+```
+PORT=
+COOKIE_KEY=
+GOOGLE_CLIENT_ID=
+GOOGLE_CLIENT_SECRET=
+GOOGLE_CALLBACK_URL=
+MONGO_URI=
+```
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,8 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
 module.exports = app => {
+
+  //--START Google Specfically Youtube account access -- //
   app.get(
     "/auth/google",
     passport.authenticate("google", {
@@ -10,21 +12,22 @@ module.exports = app => {
         "profile",
         "email"
       ]
-      //
-      //scope: ["https://www.googleapis.com/auth/plus.login","https://www.googleapis.com/auth/youtube.readonly"],
-      /*
-      scope: [
-        "https://www.googleapis.com/auth/userinfo.profile",
-        "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/youtube.readonly"
-      ]
-      */
     })
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+  //--END Google Specfically Youtube account access -- //
 
-  app.get("/api/current_user", (req, res) => {
-    res.send({user: req.user})
+
+
+  app.get("/api/logout", (req,res)=>{
+    req.logout();
+    res.send(req.user);
+  })
+
+  app.get("/api/user", (req, res) => {    
+    res.send(req.user)
   });
+
+
 };
